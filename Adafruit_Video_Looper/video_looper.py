52da173ef3f8452ed6e979eb82a1a760597d280b
mmciii <miltoncroissant@gmail.com>
2019-09-18 16:30:27 -0400

    updated message for MJM project

--- Adafruit_Video_Looper/video_looper.py
+++ Adafruit_Video_Looper/video_looper.py
@@ -160,7 +160,7 @@ class VideoLooper(object):
             return
         # Draw message with number of movies loaded and animate countdown.
         # First render text that doesn't change and get static dimensions.
-        label1 = self._render_text(message + '"Membership Community" Edition 1/3 Milton Melvin Croissant III 2014')
+        label1 = self._render_text(message + '"Atmospheric Age SIM MODE"')
         l1w, l1h = label1.get_size()
         sw, sh = self._screen.get_size()
         for i in range(seconds, 0, -1):
